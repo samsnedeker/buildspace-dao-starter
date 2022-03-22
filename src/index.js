2c23bd6142af4b0f6ac61247f4ec1e781b12be2b
Sam Snedeker <snedekersa@gmail.com>
2022-03-22 11:13:28 -0400

    connect wallet and landing page

--- src/index.js
+++ src/index.js
@@ -3,10 +3,24 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App.jsx";
 
+import { ThirdwebWeb3Provider } from '@3rdweb/hooks';
+
+// rinkeby
+const supportedChainIds = [4];
+// metamask is injected wallet
+const connectors = {
+  injected: {},
+};
+
 // Render the App component to the DOM
 ReactDOM.render(
   <React.StrictMode>
+    <ThirdwebWeb3Provider
+      connectors={connectors}
+      supportedChainIds={supportedChainIds}
+    >
     <App />
+    </ThirdwebWeb3Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
